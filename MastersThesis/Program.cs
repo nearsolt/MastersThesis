e364336140f965991ccacef8c02ee991786b469e
Andrey Veselov <veselov.andrey.sc@gmail.com>
2023-02-20 14:46:08 +0400

    Add project files.

---
+++ MastersThesis/Program.cs
@@ -0,0 +1,14 @@
+namespace MastersThesis {
+    internal static class Program {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.SetHighDpiMode(HighDpiMode.PerMonitor);
+            //Application.Run(new ());
+        }
+    }
+}
